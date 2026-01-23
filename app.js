5e9174d2b422679fa01d801fcf5e504d4a063e68
Emre Goktug Kayis <emregokktug@gmail.com>
2026-01-23 13:46:24 +0300

    Fix photo upload, add photo detail page and layout improvements

--- app.js
+++ app.js
@@ -1,24 +1,70 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
 const ejs = require('ejs');
 const path = require('path');
+const Photo = require('./models/Photo');
+const fs = require('fs');
 
 const app = express();
-//TEMPLATE ENGINE
-app.set('view engine', 'ejs');
-//MIDDLEWARES
-app.use(express.static('public'));
 
-//ROUTES
-app.get('/', (req, res) => {
-  res.render('index');
+// connect DB
+mongoose.connect('mongodb://localhost:27017/pcat-test-db');
+
+// TEMPLATE ENGINE
+app.set('view engine', 'ejs');
+
+// MIDDLEWARES
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(fileUpload());
+
+// ROUTES
+app.get('/', async (req, res) => {
+  const photos = await Photo.find({}).sort('-dateCreated');
+  res.render('index', {
+    photos,
+  });
 });
+
+app.get('/photos/:id', async (req, res) => {
+  const photo = await Photo.findById(req.params.id);
+  console.log(photo);
+  res.render('photo', {
+    photo,
+  });
+});
+
 app.get('/about', (req, res) => {
   res.render('about');
 });
+
 app.get('/add', (req, res) => {
   res.render('add');
 });
+
+app.post('/photos', async (req, res) => {
+  const uploadDir = 'public/uploads';
+
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+  }
+
+  let uploadedImage = req.files.image;
+  let uploadPath = __dirname + '/public/uploads/' + uploadedImage.name;
+
+  uploadedImage.mv(uploadPath, async () => {
+    await Photo.create({
+      ...req.body,
+      image: '/uploads/' + uploadedImage.name,
+    });
+    res.redirect('/');
+  });
+});
+
+// SERVER
 const port = 3000;
 app.listen(port, () => {
-  console.log('Sunucu' + port + ' portunda başlatıldı..');
+  console.log('Sunucu ' + port + ' portunda başlatıldı..');
 });
